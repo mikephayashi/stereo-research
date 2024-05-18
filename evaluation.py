0aea64924bb0b5be71e3eec0e525597f30cff295
Michael Hayashi <mikephayashi@gmail.com>
2024-05-17 23:49:23 -0700

    50 disparities

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io
+from justpfm import justpfm
 
 def delete_edge(ground_truth, start_idx, end_idx, axis):
     ground_truth_reduced = np.delete(ground_truth, [i for i in range(start_idx, end_idx)], axis = axis)
@@ -57,13 +60,73 @@ def get_quantiles(computed, ground_truth, max_disparity):
     res = np.quantile(absolute, probs)
     return res
 
-def evaluation_suite(computed, ground_truth, max_disparity):
+def output(f, prefix, content, results_map, results_key):
+    combined_string = ""
+    if prefix is not None:
+        combined_string = prefix + ": " + str(content)
+    else:
+        combined_string = str(content)
+    f.write(combined_string + "\n")
+    if results_map is not None:
+        results_map[results_key].append(content)
+    print(content)
+
+rms_error_val_key = "rms_error_val"
+bad_pixels_half_key = "bad_pixels_half"
+bad_pixels_one_key = "bad_pixels_one"
+bad_pixels_two_key = "bad_pixels_two"
+bad_pixels_four_key = "bad_pixels_four"
+quantiles_key = "quantiles"
+
+keys_list = [rms_error_val_key, bad_pixels_half_key, bad_pixels_one_key, bad_pixels_two_key, bad_pixels_four_key, quantiles_key]
+
+def evaluation_suite(f, computed, ground_truth, max_disparity, results_map):
     rms_error_val = rms_error(computed, ground_truth, max_disparity)
-    print(f'rms_error_val: {rms_error_val}')
-    bad_pixels = [0.5, 1, 2, 4]
-    for bad_pixel in bad_pixels:
+    output(f, "rms_error_val", rms_error_val, results_map, rms_error_val_key)
+    # bad_pixels = [0.5, 1, 2, 4]
+    bad_pixels = [(0.5, bad_pixels_half_key), (1, bad_pixels_one_key), (2, bad_pixels_two_key), (4, bad_pixels_four_key)]
+    for bad_pixel_pair in bad_pixels:
+        bad_pixel = bad_pixel_pair[0]
+        bad_pixel_key = bad_pixel_pair[1]
         bad_match_val = bad_match(computed, ground_truth, bad_pixel, max_disparity)
-        print(f'bad_match_val {bad_pixel}: {bad_match_val}')
+        output(f, "bad_match_val", bad_match_val, results_map, bad_pixel_key)
     quantiles = get_quantiles(computed, ground_truth, max_disparity)
-    print(f'quantiles: {quantiles}')
+    output(f, "quantiles", quantiles, results_map, quantiles_key)
     
+    
+if __name__ == '__main__':
+    max_disparity = 50
+    # image_names = ["Adirondack", "ArtL", "Jadeplant", "MotorcycleE", "Piano", "PianoL", "Pipes", "Playroom", "PlaytableP", "Recycle", "Shelves", "Teddy", "Vintage"]
+    image_names = ["Adirondack"]
+    base_path = "./results/slidingWindow-dotProduct/"
+    for image_name in image_names:
+        results_map = {
+            rms_error_val_key: [],
+            bad_pixels_half_key: [],
+            bad_pixels_one_key: [],
+            bad_pixels_two_key: [],
+            bad_pixels_four_key: [],
+            quantiles_key: []
+        }
+        base_results_path = base_path + "/results/"
+        f = open(base_results_path + f"{image_name}.txt", "w")
+        for disparity in range(2, max_disparity + 1):
+            combined_name = f"{image_name}-{disparity}"
+            pfm_path = base_path + f"/pfm/{combined_name}.pfm"
+            gt_pfm = justpfm.read_pfm(file_name=f"./data/gt/{image_name}/disp0GT.pfm")
+            disparity_values = justpfm.read_pfm(file_name=pfm_path)
+            output(f, None, f"{image_name}-{disparity}", None, None)
+            evaluation_suite(f, disparity_values, gt_pfm, disparity, results_map)
+            output(f, None, "", None, None)
+            # io.imshow(disparity_values)
+            # plt.show()
+        f.close()
+        for current_key in keys_list:
+            x = [i for i in range(2, max_disparity + 1)]
+            y = results_map[current_key]
+            plt.plot(x, y)
+            plt.title(current_key)  # add title
+            # plt.show()
+            plt.savefig(base_results_path + current_key)
+            plt.close()
+            
--- slidingWindow.py
+++ slidingWindow.py
@@ -1,10 +1,8 @@
 from skimage import io
-import matplotlib.pyplot as plt
 import numpy as np
 import time
 from numpy.lib.stride_tricks import sliding_window_view
 from justpfm import justpfm
-from evaluation import evaluation_suite
 
 def print_time(start):
     elaped = time.time() - start
@@ -23,7 +21,6 @@ def getWindowDotProduct(image1_windows, image2_windows, cache, disparity):
     cache[:,:,disparity] = windowDotProduct
     return windowDotProduct
 
-
 def getAbsoluteDifference(image1_windows, image2_windows, cache, disparity):
     absolute = np.abs(image1_windows - image2_windows)
     mean = np.mean(absolute, axis = (2,3))
@@ -36,52 +33,50 @@ def getSquaredDifference(image1_windows, image2_windows, cache, disparity):
     cache[:,:,disparity] = mean
     return mean
 
-start = time.time()
-image1 = io.imread("data/im0.png", as_gray=True)
-image2 = io.imread("data/im1.png", as_gray=True)
-gt_pfm = justpfm.read_pfm(file_name="./data/disp0GT.pfm")
-height, width = image1.shape
-max_disparity = 9
-half_window = 4
-window_length = 9
-window_size = (window_length, window_length)
-# cache = np.zeros(((width - 2 * half_window, height - 2 * half_window - max_disparity, max_disparity)))
-cache = np.zeros(((height - max_disparity + 1, width - max_disparity + 1, max_disparity)))
-print_time(start)
+if __name__ == '__main__':
+    max_disparity = 50
+    window_length = 9
+    window_size = (window_length, window_length)
+    # image_names = ["Adirondack", "ArtL", "Jadeplant", "MotorcycleE", "Piano", "PianoL", "Pipes", "Playroom", "PlaytableP", "Recycle", "Shelves", "Teddy", "Vintage"]
+    image_names = ["Adirondack"]
 
-'''
-Einsum
-'''
-already_computed = True
-disparity_values = None
-pfm_path = "./results/slidingWindow-squaredDifference.pfm"
-if not already_computed:
-    image1_windows = sliding_window_view(image1, window_size)
-    print_time(start)
-    image2_windows = sliding_window_view(image2, window_size)
-    print_time(start)
-    res = getSquaredDifference(image1_windows, image2_windows, cache, 0)
-    print_time(start)
-    for disparity in range(max_disparity):
-        if disparity == 0:
-            continue
-        image1, image1_windows = shiftOne(image1, image1_windows)
-        image2, image2_windows = shiftOne(image2, image2_windows)
-        res = getSquaredDifference(image1_windows, image2_windows, cache, disparity)
+    for image_name in image_names:
+        image1 = io.imread(f"data/input/{image_name}/im0.png", as_gray=True)
+        image2 = io.imread(f"data/input/{image_name}/im1.png", as_gray=True)
+        gt_pfm = justpfm.read_pfm(file_name=f"./data/gt/{image_name}/disp0GT.pfm")
+        height, width = image1.shape
+        cache = np.zeros(((height - window_length + 1, width - window_length + 1, max_disparity)))
+
+        '''
+        Einsum
+        '''
+        start = time.time()
         print_time(start)
-    disparity_values = np.argmin(cache, axis=2) 
-    disparity_values = disparity_values / (max_disparity - 1)
-    disparity_float32 = np.float32(disparity_values)      
-    justpfm.write_pfm(file_name=pfm_path, data=disparity_float32)
-    # disparity_values = np.expand_dims(disparity_values, axis=2)
-else:
-    disparity_values = justpfm.read_pfm(file_name=pfm_path)
-evaluation_suite(disparity_values, gt_pfm, max_disparity)
-io.imshow(disparity_values)
-# io.imshow(gt_pfm)
-plt.show()
+        disparity_values = None
+        image1_windows = sliding_window_view(image1, window_size)
+        print_time(start)
+        image2_windows = sliding_window_view(image2, window_size)
+        print_time(start)
+        res = getWindowDotProduct(image1_windows, image2_windows, cache, 0)
+        print_time(start)
+        for disparity in range(max_disparity):
+            if disparity == 0:
+                continue
+            image1, image1_windows = shiftOne(image1, image1_windows)
+            image2, image2_windows = shiftOne(image2, image2_windows)
+            res = getWindowDotProduct(image1_windows, image2_windows, cache, disparity)
+            print_time(start)
+        for disparity in range(2, max_disparity + 1):
+            temp_cache = cache[:,:,:disparity]
+            disparity_values = np.argmin(cache, axis=2) 
+            disparity_values = disparity_values / (disparity - 1)
+            disparity_float32 = np.float32(disparity_values)      
+            pfm_path = f"./results/slidingWindow-dotProduct/pfm/{image_name}-{disparity}.pfm"
+            justpfm.write_pfm(file_name=pfm_path, data=disparity_float32)
 
-
+'''
+Append and concatenate
+'''
 # image1_windows = np.delete(image1_windows, 0, 1)
 # elaped = time.time() - start
 # print(f"time: {elaped}")
